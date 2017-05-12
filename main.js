19c4f94ab6ea2b5acc252c6a47e06568053cec88
John Kossa <john.kossa@frogslayer.com>
2017-05-11 20:03:07 -0500

    Added a basic status check for seeing if Vanilla is up or down.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Created by Kosine on 5/11/2017.
  */
+var bashExec = require('child_process').exec;
 var express = require('express');
 var app = express();
 
@@ -8,7 +9,15 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res){
-    res.render('index',{});
+    //ps aux | grep mc_vanilla/start_server.sh | wc -l
+    bashExec("ps aux | grep mc_vanilla/start_server.sh | wc -l", function(err, stdout, stderr){
+       if(err){
+           res.render('index',{});
+       }else{
+           res.render('index',{"vanilla_status": stdout == "3"});
+       }
+    });
+
 });
 
 app.listen(80, function(){
