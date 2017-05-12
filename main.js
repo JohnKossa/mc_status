37d2d5f5bd6a92c02912734e87d5f668e74aea1c
John Kossa <john.kossa@frogslayer.com>
2017-05-11 20:17:06 -0500

    Converted project to tabs. Fixed the check to see if Vanilla is active.

--- main.js
+++ main.js
@@ -9,17 +9,20 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res){
-    //ps aux | grep mc_vanilla/start_server.sh | wc -l
-    bashExec("ps aux | grep mc_vanilla/start_server.sh | wc -l", function(err, stdout, stderr){
-       if(err){
-           res.render('index',{});
-       }else{
-           res.render('index',{"vanilla_status": stdout == "3"});
-       }
-    });
+	//ps aux | grep mc_vanilla/start_server.sh | wc -l
+	bashExec("ps aux | grep mc_vanilla/start_server.sh | wc -l", function(err, stdout, stderr){
+		console.debug("err "+JSON.stringify(err));
+		console.debug("stdout "+JSON.stringify(stdout));
+		console.debug("stderr "+JSON.stringify(stderr));
+		if(err){
+			res.render('index',{});
+		}else{
+			res.render('index',{"vanilla_status": stdout == "4\n"}); //2 for running processes, 1 for the grep command, 1 for child_process command
+		}
+	});
 
 });
 
 app.listen(5000, function(){
-    console.log("mc_server is now online and listening on port 5000");
+	console.log("mc_server is now online and listening on port 5000");
 });
